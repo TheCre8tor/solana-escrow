c78c576346cad757807543194f811aa980a77986
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-05-20 08:17:03 +0100

    Updated the lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
-pub mod entrypoint;
+pub mod error;
 pub mod instruction;
+pub mod processor;
+pub mod state;
+
+// With this macro we can turn on our entrypoint.
+#[cfg(not(feature = "no-entrypoint"))]
+pub mod entrypoint;
